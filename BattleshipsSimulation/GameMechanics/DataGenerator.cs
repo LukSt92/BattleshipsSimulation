0bf6a614dfb779c8dda8d5e3cd70172e2d932a54
LukSt92 <stach.stachu@gmail.com>
2022-07-12 20:06:56 +0200

    Added main view and GenerateBoard method.

---
+++ BattleshipsSimulation/GameMechanics/DataGenerator.cs
@@ -0,0 +1,23 @@
+﻿using BattleshipsSimulation.Models;
+
+namespace BattleshipsSimulation.GameMechanics
+{
+    public class DataGenerator
+    {
+        public static Task<bool> GenerateBoard(Player player)
+        {
+            player.Cells = new List<Cell>();
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    player.Cells.Add(new Cell() { Id = Guid.NewGuid().ToString(), X = i, Y = j });
+                }
+            }
+            return Task.FromResult(true);
+        }
+
+    }
+}
